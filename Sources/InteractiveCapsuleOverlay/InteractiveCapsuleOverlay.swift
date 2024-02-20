bfcf028dd511894493618c7949fdcf5ffe40c41b
Lonnie Gerol <lonnie@lonniegerol.com>
2024-02-20 17:28:23 -0600

    Creates Empty Swift Package

---
+++ Sources/InteractiveCapsuleOverlay/InteractiveCapsuleOverlay.swift
@@ -0,0 +1,8 @@
+//
+//  InteractiveCapsuleOverlay.swift
+//
+//
+//  Created by Lonnie Gerol on 2/20/24.
+//
+
+import Foundation
---
+++ Tests/InteractiveCapsuleOverlay/Tests.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lonnie Gerol on 2/20/24.
+//
+
+import Foundation
